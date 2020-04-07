a8d849f379e6a0adab36356925a82925a6eb75f5
Samar <63238725+samardhiman007@users.noreply.github.com>
2020-04-07 17:00:15 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,162 @@
+import pygame
+import random
+import math
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+running = True
+
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('game.png')
+pygame.display.set_icon(icon)
+
+# Background image
+background = pygame.image.load('background.jpg')
+
+# Player Image
+playerImg = pygame.image.load('game1.png')
+playerX = 370
+
+playerY = 480
+playerX_change = 0
+
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+
+
+# enemy
+
+enemyImg = []
+enemyX = []
+enemyY = []
+enemyX_change = []
+enemyY_change = []
+num_of_enemy = 6
+
+for i in range(num_of_enemy):
+    enemyImg.append(pygame.image.load('enemy.png'))
+    enemyX.append(random.randint(0, 735))
+    enemyY.append(random.randint(50, 150))
+    enemyX_change.append(4)
+    enemyY_change.append(40)
+
+# bullet
+bulletImg = pygame.image.load('bullet.png')
+bulletX = 0
+bulletY = 480
+bulletX_change = 0
+bulletY_change = 10
+bullet_state = "ready"
+
+
+def enemy(x, y, i):
+    screen.blit(enemyImg[i], (x, y))
+
+
+def fire_bullet(x, y):
+    global bullet_state
+    bullet_state = "fire"
+    screen.blit(bulletImg, (x + 16, y + 10))
+
+
+# Collision method
+def isCollision(enemyX, enemyY, bulletX, bulletY):
+    distance = math.sqrt(math.pow(enemyX - bulletX, 2)) + math.sqrt(math.pow(enemyY - bulletY, 2))
+    if distance < 27:
+        return True
+    else:
+        return False
+
+
+score_value = 0
+font = pygame.font.Font('freesansbold.ttf', 32)
+
+textX = 10
+textY = 10
+
+#Game over
+over_font = pygame.font.Font('freesansbold.ttf', 64)
+
+def game_over_text():
+   over_text = over_font.render("Game Over:" , True, (255, 255, 255))
+   screen.blit(over_text, (200, 250))
+
+def show_score(x, y):
+    score = font.render("Score:" + str(score_value), True, (255, 255, 255))
+    screen.blit(score, (x, y))
+
+
+# Game Loop
+while running:
+    screen.fill((0, 0, 0))
+    screen.blit(background, (0, 0))
+    # playerX+=0.1
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # if keys are pressed and check it is rigth or left
+    if event.type == pygame.KEYDOWN:
+        if (event.key == pygame.K_LEFT):
+            playerX_change = -5
+
+        if (event.key == pygame.K_RIGHT):
+            playerX_change = 5
+
+        if (event.key == pygame.K_SPACE):
+            if bullet_state is "ready":
+                bulletX = playerX
+                fire_bullet(bulletX, bulletY)
+
+    if (event.type == pygame.KEYUP):
+        if (event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT):
+            playerX_change = 0
+
+    # player movement
+    playerX += playerX_change
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 736:
+        playerX = 736
+
+    # Enemy movement
+    for i in range(num_of_enemy):
+        # Game over
+        if enemyY[i] > 440:
+            for j in range(num_of_enemy):
+                enemyY[j] = 2000
+            game_over_text()
+            break
+
+        enemyX[i] += enemyX_change[i]
+        if enemyX[i] <= 0:
+            enemyX_change[i] = 4
+            enemyY[i] += enemyY_change[i]
+        elif enemyX[i] >= 736:
+            enemyX_change[i] = -4
+            enemyY[i] += enemyY_change[i]
+
+        collision = isCollision(enemyX[i], enemyY[i], bulletX, bulletY)
+        if collision:
+            bulletY = 480
+            bullet_state = "ready"
+            score_value += 5
+
+            enemyX[i] = random.randint(0, 736)
+            enemyY[i] = random.randint(50, 150)
+
+        enemy(enemyX[i], enemyY[i], i)
+
+    # Bullet movement
+    if bulletY <= 0:
+        bulletY = 480
+        bullet_state = "ready"
+    if bullet_state is "fire":
+        fire_bullet(bulletX, bulletY)
+        bulletY -= bulletY_change
+
+    player(playerX, playerY)
+    show_score(textX, textY)
+    pygame.display.update()
